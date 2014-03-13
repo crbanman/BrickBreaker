3c0027dca9ac1ef2bd1b81aa3fc47c78ef1c2cde
crbanman <crbanman@gmail.com>
2014-03-12 18:03:41 -0700

    Started Game Engine
    I started developing the engine that the game will run on.

---
+++ src/ca/codybanman/brickbreaker/Game.java
@@ -0,0 +1,135 @@
+package ca.codybanman.brickbreaker;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH = 375;
+	public static final int HEIGHT = 525;
+	public static final String NAME = "BrickBreak!";
+	
+	public static Color background = Color.LIGHT_GRAY;
+
+	private JFrame frame;
+
+	public boolean running = false;
+	public int tickCount = 0;
+	public int ballX = 0;
+	public int ballY = 0;
+	public int speedX = 5;
+	public int speedY = 5;
+
+	public Game() {
+		setMinimumSize(new Dimension(WIDTH, HEIGHT));
+		setMaximumSize(new Dimension(WIDTH, HEIGHT));
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+		frame = new JFrame(NAME);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+
+	public synchronized void start() {
+		running = true;
+		new Thread(this).start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+	}
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double nsPerTick = 1000000000D / 60D;
+
+		int ticks = 0;
+		int frames = 0;
+
+		long lastTimer = System.currentTimeMillis();
+		double delta = 0;
+
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true; // set to false to limit FPS to 60
+			
+			while (delta >= 1) {
+				ticks++;
+				tick();
+				delta--;
+				shouldRender = true;
+			}
+			
+			//this is for testing purposes only. Remove before distribution.
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if (shouldRender) {
+				frames++;
+				render();
+			}
+			
+			if (System.currentTimeMillis() - lastTimer >= 1000) {
+				lastTimer += 1000;
+				System.out.println(ticks + " ticks, " + frames + " frames");
+				frames = 0;
+				ticks = 0;
+			}
+			
+		}
+	}
+
+	public void tick() {
+		if(ballX >= getWidth() - 50) speedX = -5;
+		if(ballX <= 0) speedX = 5;
+		if(ballY >= getHeight() - 50) speedY = -5;
+		if(ballY <= 0) speedY = 5;
+		ballX += speedX;
+		ballY += speedY;
+	}
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(background);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.RED);
+		
+		g.fillOval(ballX, ballY, 50, 50);
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String args[]){
+		new Game().start();
+	}
+}
